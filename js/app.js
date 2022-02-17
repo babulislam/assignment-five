83302872945d065d8f1405f313a149349c6546df
babulislamm <babul0102@gmail.com>
2022-02-18 03:51:00 +0600

    file added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// const incrementButton = document.getElementById("increment-btn");
+// incrementButton.addEventListener("click", function () { 
+// incrementButton.innerText = parseInt(incrementButton.innerText)+3;
+// });
+// console.log(hr);
+  const task = "Practice Makes a person perfect";
+  const index = task.toLowerCase().indexOf("M");
+  console.log(index);
